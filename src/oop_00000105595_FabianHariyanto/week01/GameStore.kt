52ca2dd7cd67e4001f87c04d86b1a151b1a13f25
YannnAja <fabianhariyanto06@gmail.com>
2026-02-09 01:58:29 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000105595_FabianHariyanto/week01/GameStore.kt
+++ src/oop_00000105595_FabianHariyanto/week01/GameStore.kt
@@ -3,4 +3,10 @@ package oop_00000105595_FabianHariyanto.week01
 fun main() {
     val gameTitle = "Valorant"
     val price = 300000
+
+    val finalPrice = calculateDiscount(price)
 }
+
+fun calculateDiscount(price: Int) =
+    if (price > 200000) price * 80 / 100
+    else price * 90 / 100
