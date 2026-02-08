9f8587a4a0d3f91495807f0a28e725ec47a136ce
YannnAja <fabianhariyanto06@gmail.com>
2026-02-09 01:34:30 +0700

    week01: implement null safety check

--- src/oop_00000105595_FabianHariyanto/week01/GradingSystem.kt
+++ src/oop_00000105595_FabianHariyanto/week01/GradingSystem.kt
@@ -7,7 +7,7 @@ fun main() {
     println("Nama:$name, Nilai: $score")
 
     // Tambahkan di bawah println
-    val grade when = (score) {
+    val grade = when (score) {
         in 90..100 -> "A"
         in 80..89 -> "B"
         in 70..79 -> "C"
