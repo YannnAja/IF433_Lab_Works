cce00e9e737553d4f3e81dc4b7b8972404451579
YannnAja <fabianhariyanto06@gmail.com>
2026-03-04 17:13:47 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_00000105595_FabianHariyanto/week02/Student.kt
+++ src/oop_00000105595_FabianHariyanto/week02/Student.kt
@@ -1,2 +1,20 @@
 package oop_00000105595_FabianHariyanto.week02
 
+class Student (
+    val name: String,
+    val nim: String,
+    var gpa: Double = 0.0,
+    var major: String
+){
+    init {
+        if (nim.length != 5) {
+            println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!");
+            println("Data mahasiswa $name mungkin akan bermasalah di sistem.");
+        } else {
+            println("LOG: Objek Student $name berhasil dialokasikan di Memory.");
+        }
+    }
+    constructor (name: String, nim: String) : this (name, nim, "Non-Matriculated") {
+        println("LOG: Menggunakan constructor jalur umum (tanpa jurusan)");
+    }
+}
