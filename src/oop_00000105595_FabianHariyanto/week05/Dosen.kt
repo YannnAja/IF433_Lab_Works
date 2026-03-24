3a75d70ed7c3ae4db256be297f56ec734dc44bb8
YannnAja <fabianhariyanto06@gmail.com>
2026-03-24 20:19:51 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/oop_00000105595_FabianHariyanto/week05/Dosen.kt
+++ src/oop_00000105595_FabianHariyanto/week05/Dosen.kt
@@ -1,2 +1,12 @@
 package oop_00000105595_FabianHariyanto.week05
 
+class Dosen(nama: String, val nidn: String) : Pegawai(nama) {
+
+    override fun bekerja() {
+        println("[$nama] sedang menyiapkan materi perkuliahan dan merevisi RPKPS.")
+    }
+
+    fun mengajar() {
+        println("[$nama] sedang mengajar mahasiswa di kelas.")
+    }
+}
--- src/oop_00000105595_FabianHariyanto/week05/Main.kt
+++ src/oop_00000105595_FabianHariyanto/week05/Main.kt
@@ -1,2 +1,31 @@
 package oop_00000105595_FabianHariyanto.week05
 
+fun main() {
+    val dosen1 = Dosen(nama = "Pak Alex", nidn = "0123456")
+    val admin1 = Admin(nama = "Bu Siti")
+
+    val daftarPegawai: List<Pegawai> = listOf(dosen1, admin1)
+
+    println("=== AKTIVITAS PEGAWAI ===")
+    for (pegawai in daftarPegawai) {
+        pegawai.bekerja()
+
+        when (pegawai) {
+            is Dosen -> {
+                println("=> Terdeteksi sebagai Dosen (NIDN: ${pegawai.nidn})")
+                pegawai.mengajar()
+            }
+            is Admin -> {
+                println("=> Terdeteksi sebagai Admin")
+                pegawai.doAdminWork()
+            }
+        }
+        println("------------------------------------------------")
+    }
+
+    println("\n=== TUGAS 1: MathHelper  ===")
+    val math = MathHelper()
+    println("Luas Persegi (5): ${math.hitungLuas(5)}")
+    println("Luas Persegi Panjang (4 x 6): ${math.hitungLuas(4, 6)}")
+    println("Luas Lingkaran (r 7.0): ${math.hitungLuas(7.0)}")
+}
