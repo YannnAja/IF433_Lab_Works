6e246c7f0ad75eb04ed390d43a9c97e84198bda0
YannnAja <fabianhariyanto06@gmail.com>
2026-03-24 18:49:40 +0700

    week03: (task 2) implement Player testing in main

--- src/oop_00000105595_FabianHariyanto/week03/Main.kt
+++ src/oop_00000105595_FabianHariyanto/week03/Main.kt
@@ -16,4 +16,15 @@ fun main() {
     sword.damage = -50
     sword.damage = 9999
     println("Senjata: ${sword.name}, Damage: ${sword.damage}, Tier: ${sword.tier}")
+
+    println("\n--- TUGAS 2: PLAYER ---")
+    val p1 = Player("Yann")
+    //println(p1.xp) // Akan error private
+    println("Level awal ${p1.username}: ${p1.level}")
+
+    p1.addXp(50)
+    println("Level setelah tambah 50 XP: ${p1.level}")
+
+    p1.addXp(60)
+    println("Level saat ini: ${p1.level}")
 }
--- src/oop_00000105595_FabianHariyanto/week03/Player.kt
+++ src/oop_00000105595_FabianHariyanto/week03/Player.kt
@@ -1,2 +1,20 @@
 package oop_00000105595_FabianHariyanto.week03
 
+class Player(val username: String) {
+    private var xp: Int = 0
+
+    val level: Int
+        get() = (xp / 100) + 1
+
+    fun addXp(amount: Int) {
+        if (amount > 0) {
+            val oldLevel = this.level
+            xp += amount
+            val newLevel = this.level
+
+            if (newLevel > oldLevel) {
+                println("Level Up! Selamat $username naik ke level $newLevel")
+            }
+        }
+    }
+}
