4ee362f06bfab5ce351011a3cb67f59c800c40f9
YannnAja <fabianhariyanto06@gmail.com>
2026-06-11 23:20:18 +0700

    week13: (task) implement CSV header initialization

--- src/oop_00000105595_FabianHariyanto/week13/TradeFileHandler.kt
+++ src/oop_00000105595_FabianHariyanto/week13/TradeFileHandler.kt
@@ -4,4 +4,10 @@ import java.io.File
 
 object TradeFileHandler {
     private val file = File("trades.csv")
+
+    fun initHeader() {
+        if (!file.exists()) {
+            file.writeText("Pair,Type,Price,PnL\n")
+        }
+    }
 }
