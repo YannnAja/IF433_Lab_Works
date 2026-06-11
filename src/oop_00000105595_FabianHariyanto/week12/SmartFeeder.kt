7f87ca430081a5e6c83874920deaee88f7018e3f
YannnAja <fabianhariyanto06@gmail.com>
2026-06-11 23:02:34 +0700

    week12: (task) handle feeding success using onSuccess

--- src/oop_00000105595_FabianHariyanto/week12/SmartFeeder.kt
+++ src/oop_00000105595_FabianHariyanto/week12/SmartFeeder.kt
@@ -40,4 +40,8 @@ fun main() {
     val feedingResult = runCatching {
         dispenseKibble(requestedGram = 30, availableGram = 1000, isJammed = false)
     }
+    feedingResult.onSuccess { newStock ->
+        currentKibbleStock = newStock
+        println("Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr")
+    }
 }
