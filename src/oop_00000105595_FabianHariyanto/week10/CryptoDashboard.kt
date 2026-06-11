0c70e4d396916080d2fbba73ddc2c9b10dac85d6
YannnAja <fabianhariyanto06@gmail.com>
2026-06-11 19:45:10 +0700

    week10: (task) populate coin repository

--- src/oop_00000105595_FabianHariyanto/week10/CryptoDashboard.kt
+++ src/oop_00000105595_FabianHariyanto/week10/CryptoDashboard.kt
@@ -3,4 +3,8 @@ package oop_00000105595_FabianHariyanto.week10
 fun main() {
     val coinRepo = WalletRepository<Coin>()
     println("=== CRYPTO DASHBOARD ===")
+
+    coinRepo.add(Coin("BTC", 1.5))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 500.0))
 }
