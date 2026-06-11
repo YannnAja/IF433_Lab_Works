81fa626d734d782f9bbf53faa33d855385d19a9c
YannnAja <fabianhariyanto06@gmail.com>
2026-06-11 22:51:23 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_00000105595_FabianHariyanto/week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_00000105595_FabianHariyanto.week11
+
+data class SmartDevice(
+    var name: String = "",
+    var category: String = "",
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
