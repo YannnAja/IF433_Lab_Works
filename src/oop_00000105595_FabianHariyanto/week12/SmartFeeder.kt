9691606ee995c2b5bf68b5eef8a8d2c733da3e10
YannnAja <fabianhariyanto06@gmail.com>
2026-06-11 23:02:07 +0700

    week12: (task) execute feeding using runCatching

--- src/oop_00000105595_FabianHariyanto/week12/SmartFeeder.kt
+++ src/oop_00000105595_FabianHariyanto/week12/SmartFeeder.kt
@@ -35,4 +35,9 @@ fun main() {
     } finally {
         println("Siklus pengecekan dispenser pagi selesai.")
     }
+
+    println("\n[Jadwal Makan Sore]")
+    val feedingResult = runCatching {
+        dispenseKibble(requestedGram = 30, availableGram = 1000, isJammed = false)
+    }
 }
