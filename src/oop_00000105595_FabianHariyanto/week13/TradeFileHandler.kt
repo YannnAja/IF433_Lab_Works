bf8931fe517f819bb801494111802b1071d268ff
YannnAja <fabianhariyanto06@gmail.com>
2026-06-11 23:21:38 +0700

    week13: (task) implement loadTrades deserialization logic

--- src/oop_00000105595_FabianHariyanto/week13/TradeFileHandler.kt
+++ src/oop_00000105595_FabianHariyanto/week13/TradeFileHandler.kt
@@ -14,4 +14,16 @@ object TradeFileHandler {
     fun saveTrade(trade: TradeRecord) {
         file.appendText("${trade.pair},${trade.type},${trade.price},${trade.pnl}\n")
     }
+
+    fun loadTrades(): List<TradeRecord> {
+        if (!file.exists()) return emptyList()
+
+        return file.useLines { lines ->
+            lines.drop(1) // Skip baris pertama (Header CSV)
+                .map { line ->
+                    val parts = line.split(",")
+                    TradeRecord(parts[0], parts[1], parts[2].toDouble(), parts[3].toDouble())
+                }.toList()
+        }
+    }
 }
