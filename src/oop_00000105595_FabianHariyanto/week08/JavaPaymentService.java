f1679855fe1e5820b4665367e3117b45c144e011
YannnAja <fabianhariyanto06@gmail.com>
2026-06-11 18:48:34 +0700

    week08: (task) create Java Payment Service for payment interop

---
+++ src/oop_00000105595_FabianHariyanto/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000105595_FabianHariyanto.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
